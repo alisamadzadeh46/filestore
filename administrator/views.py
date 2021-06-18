5e9120c689661460341ae55f955b82dbf1160bf4
alisamadzadeh46 <alisamadzadeh46@gmail.com>
2021-06-18 22:28:55 +0430

    update

--- administrator/views.py
+++ administrator/views.py
@@ -1,7 +1,1 @@
 
-from rest_framework.views import APIView
-
-
-class RegisterAPIView(APIView):
-    def post(self, request):
-        pass
--- common/urls.py
+++ common/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from administrator.views import RegisterAPIView
+from .views import RegisterAPIView
 
 app_name = 'common'
 
