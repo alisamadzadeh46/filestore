c09d6754506100990430b9d8b4b3e2c61e22a819
alisamadzadeh46 <alisamadzadeh46@gmail.com>
2021-06-18 21:25:45 +0430

    create class RegisterAPIView

--- filestore/urls.py
+++ filestore/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/admin/', include('administrator.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
