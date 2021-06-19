d384fcfc6622fe2ebc3b23d73349d2df3908710b
alisamadzadeh46 <alisamadzadeh46@gmail.com>
2021-06-19 19:13:33 +0430

    update

--- common/urls.py
+++ common/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import RegisterAPIView
+from .views import *
 
 app_name = 'common'
 
 urlpatterns = [
     path('register', RegisterAPIView.as_view(), name='register'),
+    path('login', LoginAPIView.as_view(),name='login'),
 ]
--- common/views.py
+++ common/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework import exceptions
 from rest_framework.response import Response
 from .serializers import UserSerializer
+from core.models import User
 
 
 class RegisterAPIView(APIView):
@@ -17,3 +18,32 @@ class RegisterAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Incorrect Password!')
+
+        scope = 'ambassador' if 'api/ambassador' in request.path else 'admin'
+
+        if user.is_ambassador and scope == 'admin':
+            raise exceptions.AuthenticationFailed('Unauthorized')
+
+        token = JWTAuthentication.generate_jwt(user.id, scope)
+
+        response = Response()
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'message': 'success'
+        }
+
+        return response
