4396bb76598f4b5141f09ec575c0ddea2b3c25f2
alisamadzadeh46 <alisamadzadeh46@gmail.com>
2021-06-20 02:06:38 +0430

    update views.py create serializer.py

--- ambassador/views.py
+++ ambassador/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+import math, random, time, string
 
-# Create your views here.
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from common.authentication import JWTAuthentication
+from common.serializers import UserSerializer
+from .serializer import ProductSerializer, LinkSerializer
+from core.models import Product, Link, Order, User
+from django.core.cache import cache
 
 
 class ProductFrontendAPIView(APIView):
-    pass
+    @method_decorator(cache_page(60 * 60 * 2, key_prefix='products_frontend'))
+    def get(self, _):
+        time.sleep(2)
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
 
 class ProductBackendAPIView(APIView):
