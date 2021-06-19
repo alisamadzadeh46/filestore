3518744d578da0df7e2b3549465615ee2f35c4b9
alisamadzadeh46 <alisamadzadeh46@gmail.com>
2021-06-19 20:11:01 +0430

    update urls.py

--- administrator/urls.py
+++ administrator/urls.py
@@ -6,4 +6,6 @@ app_name = 'administrator'
 urlpatterns = [
     path('', include('common.urls')),
     path('ambassadors', AmbassadorAPIView.as_view(), name='ambassadors'),
+    path('products', ProductGenericAPIView.as_view(), name='products'),
+    path('products/<str:pk>', ProductGenericAPIView.as_view(), name='products'),
 ]
