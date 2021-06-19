19b71ad8c4f85a24af61b83daee54197bc811620
alisamadzadeh46 <alisamadzadeh46@gmail.com>
2021-06-20 02:04:09 +0430

    update urls.py create populate_orders

--- ambassador/views.py
+++ ambassador/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+
+
+class ProductFrontendAPIView(APIView):
+    pass
+
+
+class ProductBackendAPIView(APIView):
+    pass
+
+
+class LinkAPIView(APIView):
+    pass
+
+
+class StatsAPIView(APIView):
+    pass
+
+
+class RankingsAPIView(APIView):
+    pass
