33d82706e93cc9ade7607c936e8361f676f22c0e
alisamadzadeh46 <alisamadzadeh46@gmail.com>
2021-06-19 19:50:37 +0430

    update urls.py

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,8 +1,9 @@
-
 from django.urls import path, include
+from .views import *
 
 app_name = 'administrator'
 
 urlpatterns = [
     path('', include('common.urls')),
+    path('ambassadors', AmbassadorAPIView.as_view(), name='ambassadors'),
 ]
