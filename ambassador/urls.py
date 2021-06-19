a72ccebd72cfe8914d8b6c46e1c54b6ab0577086
alisamadzadeh46 <alisamadzadeh46@gmail.com>
2021-06-20 01:54:48 +0430

    update urls.py

--- ambassador/urls.py
+++ ambassador/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 
 from .views import ProductFrontendAPIView, ProductBackendAPIView, LinkAPIView, StatsAPIView, RankingsAPIView
 
+app_name = 'ambassador'
+
 urlpatterns = [
     path('', include('common.urls')),
-    path('products/frontend', ProductFrontendAPIView.as_view()),
-    path('products/backend', ProductBackendAPIView.as_view()),
-    path('links', LinkAPIView.as_view()),
-    path('stats', StatsAPIView.as_view()),
-    path('rankings', RankingsAPIView.as_view()),
+    path('products/frontend', ProductFrontendAPIView.as_view(), name='product_frontend'),
+    path('products/backend', ProductBackendAPIView.as_view(), name='product_backend'),
+    path('links', LinkAPIView.as_view(), name='links'),
+    path('stats', StatsAPIView.as_view(), name='stats'),
+    path('rankings', RankingsAPIView.as_view(), name='rankings'),
 ]
