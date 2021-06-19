e956edcb5745c574cefe42442a9dee7deb5cb1f2
alisamadzadeh46 <alisamadzadeh46@gmail.com>
2021-06-20 02:07:58 +0430

    update views.py

--- ambassador/views.py
+++ ambassador/views.py
@@ -23,7 +23,43 @@ class ProductFrontendAPIView(APIView):
 
 
 class ProductBackendAPIView(APIView):
-    pass
+
+    def get(self, request):
+        products = cache.get('products_backend')
+        if not products:
+            time.sleep(2)
+            products = list(Product.objects.all())
+            cache.set('products_backend', products, timeout=60 * 30)  # 30 min
+
+        s = request.query_params.get('s', '')
+        if s:
+            products = list([
+                p for p in products
+                if (s.lower() in p.title.lower()) or (s.lower() in p.description.lower())
+            ])
+
+        total = len(products)
+
+        sort = request.query_params.get('sort', None)
+        if sort == 'asc':
+            products.sort(key=lambda p: p.price)
+        elif sort == 'desc':
+            products.sort(key=lambda p: p.price, reverse=True)
+
+        per_page = 9
+        page = int(request.query_params.get('page', 1))
+        start = (page - 1) * per_page
+        end = page * per_page
+
+        data = ProductSerializer(products[start:end], many=True).data
+        return Response({
+            'data': data,
+            'meta': {
+                'total': total,
+                'page': page,
+                'last_page': math.ceil(total / per_page)
+            }
+        })
 
 
 class LinkAPIView(APIView):
