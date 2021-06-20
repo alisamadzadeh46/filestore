d546a694772853f98925b57570589646c35e9867
alisamadzadeh46 <alisamadzadeh46@gmail.com>
2021-06-20 22:01:57 +0430

    update settings.py

--- filestore/settings.py
+++ filestore/settings.py
@@ -92,6 +92,16 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6378/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
